62b26cd85c4442ff0493f67b93327e87e21849ec
Tim Pietrusky <timpietrusky@googlemail.com>
2012-12-16 00:58:52 +0100

    Improved quality.

--- js/emocons.js
+++ js/emocons.js
@@ -3,16 +3,16 @@
     var emoticons = {
         1 : { text: "emo-happy", regex: ":\\)", emo : ":)"},
         2 : { text: "emo-grin", regex: "\:\D", emo : ":D"},
-          3 : { text: "emo-wink", regex: ";\\)", emo : ";)"},
+        3 : { text: "emo-wink", regex: ";\\)", emo : ";)"},
         4 : { text: "emo-cry", regex: ":'\\(", emo : ":'("},
-          5 : { text: "emo-surprised", regex: ":o", emo : ":o"},
+        5 : { text: "emo-surprised", regex: ":o", emo : ":o"},
         6 : { text: "emo-displeased", regex: ":/", emo : ":/"}, 
         7 : { text: "emo-devil", regex: "\:devil\:", emo : ":devil:"}, 
-          8 : { text: "emo-unhappy", regex: ":\\(", emo : ":("},
-          9 : { text: "emo-sunglasses", regex: "B\\)", emo : "B)"},
-          10 : { text: "emo-tongue", regex: ":P", emo : ":P"},
+        8 : { text: "emo-unhappy", regex: ":\\(", emo : ":("},
+        9 : { text: "emo-sunglasses", regex: "B\\)", emo : "B)"},
+        10 : { text: "emo-tongue", regex: ":P", emo : ":P"},
         11 : { text: "emo-beer", regex: ":beer:", emo : ":beer:"},
-          12 : { text: "emo-sleep", regex: ":\\|", emo : ":|"},
+        12 : { text: "emo-sleep", regex: ":\\|", emo : ":|"},
         13 : { text: "emo-shoot", regex: "\:shoot\:", emo : ":shoot:"},
         14 : { text: "emo-coffee", regex: "\:coffee\:", emo : ":coffee:"},
         15 : { text: "emo-thumbsup", regex: "\:thumbsup\:", emo : ":thumbsup:"},
@@ -25,9 +25,9 @@
       var el = $(this);
       
       /*
-              * I need to do this because CodePen executes 
+       * I need to do this because CodePen executes 
        * my JavaScript twice.
-             */
+       */
       if (!el.hasClass('done')) {
         el.addClass('done');
           
